54be4920114bdf058cbb6e583693f7ecb5aea03b
hendridg <hendridg@gmail.com>
2022-01-12 01:50:08 -0400

    Delete task element

--- src/index.js
+++ src/index.js
@@ -19,47 +19,47 @@ const arrayTasks = [
   },
 ];
 
-function Task(task) {
-  let isSelected = false;
-  const li = document.createElement('li');
-  const itemTaskElement = document.createElement('div');
-  const checkBox = document.createElement('input');
-  const checkBoxContainer = document.createElement('div');
-  checkBoxContainer.classList.add('checkbox-container');
-  const label = document.createElement('label');
-  const iconMenu = document.createElement('div');
-  const iconDelete = document.createElement('div');
-  iconMenu.classList.add('icon-menu-container');
-  itemTaskElement.classList.add('item-element');
-  iconMenu.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-  iconDelete.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  checkBox.type = 'checkbox';
-  checkBox.id = `check${task.index}`;
-  label.htmlFor = `check${task.index}`;
-  label.innerText = task.description;
+// function Task(task) {
+//   let isSelected = false;
+//   const li = document.createElement('li');
+//   const itemTaskElement = document.createElement('div');
+//   const checkBox = document.createElement('input');
+//   const checkBoxContainer = document.createElement('div');
+//   checkBoxContainer.classList.add('checkbox-container');
+//   const label = document.createElement('label');
+//   const iconMenu = document.createElement('div');
+//   const iconDelete = document.createElement('div');
+//   iconMenu.classList.add('icon-menu-container');
+//   itemTaskElement.classList.add('item-element');
+//   iconMenu.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+//   iconDelete.innerHTML = '<i class="fas fa-trash-alt"></i>';
+//   checkBox.type = 'checkbox';
+//   checkBox.id = `check${task.index}`;
+//   label.htmlFor = `check${task.index}`;
+//   label.innerText = task.description;
 
-  label.addEventListener('click', () => {
-    isSelected = !isSelected;
-    if (isSelected === true) {
-      label.style.textDecoration = 'line-through';
-    } else {
-      label.style.textDecoration = 'none';
-    }
-  });
+//   label.addEventListener('click', () => {
+//     isSelected = !isSelected;
+//     if (isSelected === true) {
+//       label.style.textDecoration = 'line-through';
+//     } else {
+//       label.style.textDecoration = 'none';
+//     }
+//   });
 
-  checkBox.addEventListener('click', () => {
-    if (checkBox.checked) {
-      label.style.textDecoration = 'line-through';
-    } else {
-      label.style.textDecoration = 'none';
-    }
-  });
+//   checkBox.addEventListener('click', () => {
+//     if (checkBox.checked) {
+//       label.style.textDecoration = 'line-through';
+//     } else {
+//       label.style.textDecoration = 'none';
+//     }
+//   });
 
-  checkBoxContainer.append(checkBox, label);
-  itemTaskElement.append(checkBoxContainer, iconMenu);
-  li.appendChild(itemTaskElement);
-  return li;
-}
+//   checkBoxContainer.append(checkBox, label);
+//   itemTaskElement.append(checkBoxContainer, iconMenu);
+//   li.appendChild(itemTaskElement);
+//   return li;
+// }
 
 function component() {
   const containerElement = document.createElement('div');
@@ -80,10 +80,10 @@ function component() {
   btnClearTasks.classList.add('btn-clear-task');
   btnClearTasks.disabled = true;
 
-  arrayTasks.map((task) => {
-    ulElement.appendChild(Task(task));
-    return 'done';
-  });
+  //   arrayTasks.map((task) => {
+  //     ulElement.appendChild(Task(task));
+  //     return 'done';
+  //   });
 
   containerElement.append(titleElement, inputElement, ulElement, btnClearTasks);
 
